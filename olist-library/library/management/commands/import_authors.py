3491f62f359e49b816d373896aa37f51fc2baabb
Igor Xavier <igoorlimaa@gmail.com>
2020-06-11 15:40:45 -0300

    Add import_authors management command

---
+++ olist-library/library/management/commands/import_authors.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from library.models import Author
+import csv
+
+
+class Command(BaseCommand):
+    help = "Imports the given csv data into the Authors table"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file", type=str)
+
+    def handle(self, *args, **options):
+        try:
+            file = open(options["file"])
+        except FileNotFoundError:
+            raise CommandError("File not found")
+        except TypeError:
+            raise CommandError("A csv type file is required")
+
+        reader = csv.reader(file)
+
+        for i, row in enumerate(reader):
+            # Ignores the first row of csv, because its a header
+            if i > 0:
+                Author.objects.create(name=row[0])
+
+        self.stdout.write(self.style.SUCCESS(f"Successfully imported {reader.line_num - 1} author(s) from CSV file"))
