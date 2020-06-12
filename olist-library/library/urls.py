ae1d47981c7e4626f293f50d9f93c370d5f1e432
Igor Xavier <igoorlimaa@gmail.com>
2020-06-12 19:57:38 -0300

    Add app_name and basename to Library api urls

--- olist-library/library/urls.py
+++ olist-library/library/urls.py
@@ -3,9 +3,11 @@ from rest_framework import routers
 from library import views
 
 
+app_name = "library"
+
 router = routers.DefaultRouter()
-router.register(r"authors", views.AuthorViewSet)
-router.register(r"books", views.BookViewSet)
+router.register(r"authors", views.AuthorViewSet, basename="authors")
+router.register(r"books", views.BookViewSet, basename="books")
 
 
 urlpatterns = [
