b0e02f791533a1c8a00f014a00f1d3f65c637d3b
Igor Xavier <igoorlimaa@gmail.com>
2020-06-12 21:23:06 -0300

    Change Author and Books serializers: add id field

--- olist-library/library/serializers.py
+++ olist-library/library/serializers.py
@@ -6,10 +6,10 @@ from library.models import Book, Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ["name"]
+        fields = ["id", "name"]
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["name", "edition", "publication_year", "authors"]
+        fields = ["id", "name", "edition", "publication_year", "authors"]
