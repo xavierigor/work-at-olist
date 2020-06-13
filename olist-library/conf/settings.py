b546a416c46498190c764c7dde02a803c5add485
Igor Xavier <igoorlimaa@gmail.com>
2020-06-12 22:01:03 -0300

    Alter database config in order to work with Heroku

--- olist-library/conf/settings.py
+++ olist-library/conf/settings.py
@@ -3,6 +3,7 @@ import os
 import environ
 
 import django_heroku
+import dj_database_url
 
 env = environ.Env()
 root_path = environ.Path(__file__) - 2
@@ -134,3 +135,4 @@ REST_FRAMEWORK = {
 # Django Heroku configuration
 # -----------------------------------------------------------------------------
 django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
